288c34f40350d8c79a91ab69e220e45acd60423b
srijani <bhattasrijani75@gmail.com>
2023-04-02 20:38:22 -0400

    finish project

--- app/src/main/java/com/finalyearproj/tujour/Common/SplashScreen.java
+++ app/src/main/java/com/finalyearproj/tujour/Common/SplashScreen.java
@@ -67,7 +67,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },5000);
+        },3000);
 
     }
 }
