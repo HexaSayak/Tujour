80eb20838ac943c5f3755224346ad00f983c0ccf
sayak <sayakghosh24012001@gmail.com>
2023-04-05 12:27:34 +0530

    Commit5_4_23_12:27!

--- app/src/main/java/com/finalyearproj/tujour/User/UserDashboard.java
+++ app/src/main/java/com/finalyearproj/tujour/User/UserDashboard.java
@@ -3,6 +3,8 @@ package com.finalyearproj.tujour.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.finalyearproj.tujour.R;
 
@@ -11,6 +13,7 @@ public class UserDashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
     }
 }
