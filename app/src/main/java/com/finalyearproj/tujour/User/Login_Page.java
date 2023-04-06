d0b380de42313b614434619d6d7f56d6a9563c88
Rohitbabu-7872 <120325094+Rohitbabu-7872@users.noreply.github.com>
2023-04-07 05:02:10 +0530

    new activity

--- app/src/main/java/com/finalyearproj/tujour/User/Login_Page.java
+++ app/src/main/java/com/finalyearproj/tujour/User/Login_Page.java
@@ -1,5 +1,6 @@
 package com.finalyearproj.tujour.User;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +11,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.finalyearproj.tujour.R;
 
-public class Loginpage extends AppCompatActivity {
+public class Login_Page extends AppCompatActivity {
 
     EditText username;
     EditText password;
     Button loginButton;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.activity_login_page);
 
         username=findViewById(R.id.username);
         password=findViewById(R.id.password);
@@ -27,9 +29,9 @@ public class Loginpage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("RohitBabu7872") && password.getText().toString().equals("7872")) {
-                    Toast.makeText(Loginpage.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login_Page.this, "Login successful", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(Loginpage.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login_Page.this, "Login failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
